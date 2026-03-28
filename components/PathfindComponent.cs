bb52d49b3d7f4e926e270b95e9e9ad8c802eca40
aaikon <arttu.ikonen@outlook.com>
2026-03-29 01:12:01 +0200

    feat: add a PathfindComponent for moving around terrain and objects

---
+++ components/PathfindComponent.cs
@@ -0,0 +1,31 @@
+using Godot;
+
+namespace Game.component
+{
+  [GlobalClass]
+  public partial class PathfindComponent : Node2D
+  {
+    [Export]
+    private VelocityComponent velocityComponent;
+    [Export]
+    public NavigationAgent2D NavigationAgent2D;
+
+    public void SetTargetPosition(Vector2 position)
+    {
+      NavigationAgent2D.TargetPosition = position;
+    }
+
+    public void FollowPath()
+    {
+      if (NavigationAgent2D.IsNavigationFinished())
+      {
+        velocityComponent.Decelerate();
+        return;
+      }
+
+      var direction = (NavigationAgent2D.GetNextPathPosition() - GlobalPosition).Normalized();
+
+      velocityComponent.AccelerateInDirection(direction);
+    }
+  }
+}
