697a56ffc45b31e861553684fcbe362a2ac24d25
aaikon <arttu.ikonen@outlook.com>
2026-03-28 19:13:11 +0200

    feat: add a VelocityComponent for handling unit velocity

---
+++ components/VelocityComponent.cs
@@ -0,0 +1,54 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Game.component
+{
+  [GlobalClass]
+  public partial class VelocityComponent : Node
+  {
+    [Export]
+    private float maxSpeed = 100;
+    [Export]
+    private float acceleration = 10;
+
+    public Vector2 Velocity { get; set; }
+    public Vector2? VelocityOverride { get; set; }
+    public float SpeedMultiplier { get; set; } = 1.0f;
+    public float SpeedPercentModifier => speedPercentModifiers.Values.Sum();
+    public float AccelerationMultiplier { get; set; } = 1.0f;
+
+    public float Acceleration => acceleration;
+    public float SpeedPercent => Math.Min(Velocity.Length() / (CalculatedMaxSpeed > 0f ? CalculatedMaxSpeed : 1f), 1f);
+    public float CalculatedMaxSpeed => maxSpeed * (1f + SpeedPercentModifier) * SpeedMultiplier;
+
+    private Dictionary<string, float> speedPercentModifiers = new();
+
+    public void AccelerateToVelocity(Vector2 velocity)
+    {
+      Velocity = Velocity.Lerp(velocity, 1f - Mathf.Exp(-acceleration * AccelerationMultiplier));
+    }
+
+    public void AccelerateInDirection(Vector2 direction)
+    {
+      AccelerateToVelocity(direction.Normalized() * CalculatedMaxSpeed);
+    }
+
+    public void Decelerate()
+    {
+      AccelerateToVelocity(Vector2.Zero);
+    }
+
+    public void Move(CharacterBody2D characterBody)
+    {
+      characterBody.Velocity = VelocityOverride ?? Velocity;
+      characterBody.MoveAndSlide();
+    }
+
+    public void AddSpeedPercentModifier(string name, float modifier)
+    {
+      speedPercentModifiers[name] = modifier;
+    }
+  }
+}
--- fella/Fella.cs
+++ fella/Fella.cs
@@ -1,8 +1,11 @@
+using Game.component;
 using Godot;
 
-namespace Game.component
+namespace Game.character
 {
   public partial class Fella : CharacterBody2D
   {
+	[Export]
+	private VelocityComponent velocityComponent;
   }
 }
